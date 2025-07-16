712fe9e1277c195f726980db63080ffbf10cf231
Pedro Falfan <pe.falfan@duocuc.cl>
2025-07-16 00:11:24 -0400

    Added CORS config

---
+++ src/main/java/cl/biblioteca/otaku/bff/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package cl.biblioteca.otaku.bff.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://biblioteca-otaku-c6329.web.app", "http://localhost:4200")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
